38f64b002b27305ae8408431451b18b3ac5996e5
Aswankappingal <instamagic774@gmail.com>
2024-06-19 13:13:44 +0530

    ppp

--- My-project/src/Components/Background/Background.jsx
+++ My-project/src/Components/Background/Background.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import './Background.scss'
 
 const Background = () => {
-  return (
-    <div>
+    return (
+        <div>
 
-        <div className="main-bg">
-        <div className="hero-content">
-    <h1>Welcome to Berger Hut</h1>
-    <p>Experience the Taste of Perfection</p>
-    <a href="#menu" className="btn">Explore Our Menu</a>
-  </div>
+            <div className="main-bg">
+                <div className="hero-content">
+                    <h1>Welcome to Berger Hut</h1>
+                    <p>Experience the Taste of Perfection</p>
+                    <a href="#menu" className="btn">Explore Our Menu</a>
+                </div>
+
+            </div>
 
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Background
