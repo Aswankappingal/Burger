55160d2faaf2fcd9476ee98b84b5d14addf735ee
Aswankappingal <instamagic774@gmail.com>
2024-06-19 13:12:48 +0530

    yes

--- My-project/src/Components/Background/Background.jsx
+++ My-project/src/Components/Background/Background.jsx
@@ -6,6 +6,11 @@ const Background = () => {
     <div>
 
         <div className="main-bg">
+        <div className="hero-content">
+    <h1>Welcome to Berger Hut</h1>
+    <p>Experience the Taste of Perfection</p>
+    <a href="#menu" className="btn">Explore Our Menu</a>
+  </div>
 
         </div>
       
