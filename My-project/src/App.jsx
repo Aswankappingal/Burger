88c9cf1e372b0d9ccfc300fb05818cc9476955d9
Aswankappingal <instamagic774@gmail.com>
2024-06-19 12:15:23 +0530

    file is addded

---
+++ My-project/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import  { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Page from './Components/Justpage/Page'
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' Component={Page}>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+   
+    </>
+  )
+}
+
+export default App
---
+++ My-project/src/Components/Justpage/Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Page.scss'
+
+const Page = () => {
+  return (
+    <div>
+        <div className="bg-main">
+            <div className="head">
+                 <h3>Data</h3>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Page
