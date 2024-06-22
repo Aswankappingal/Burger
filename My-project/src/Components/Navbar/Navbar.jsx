a108038bbda8cbaf10b608f50f8566245fadd1bc
Aswankappingal <instamagic774@gmail.com>
2024-06-22 13:11:46 +0530

    pppp

--- My-project/src/Components/Navbar/Navbar.jsx
+++ My-project/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Navbar.scss'
+import { Link } from 'react-router-dom'
+import About from '../About/About'
 
 const Navbar = () => {
     return (
@@ -18,7 +20,9 @@ const Navbar = () => {
 
                         <ul>
                             <li><a href="#home">Home</a></li>
-                            <li><a href="#about">About</a></li>
+                           <Link to={About}> 
+                           <li><a href="About">About</a></li>
+                           </Link>
                             <li><a href="#menu">Menu</a></li>
                             <li><a href="#reservations">Reservations</a></li>
                             <li><a href="#contact">Contact</a></li>
